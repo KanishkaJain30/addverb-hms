7f66ca901915ced31beccb71cc292586ef751f8c
Khyati Goyal <17uec065@lnmiit.ac.in>
2020-10-16 14:58:17 +0530

    dummy

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,5 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  'mongodb+srv://khyatigoyal:khyatigoyal@cluster0.66s8n.mongodb.net/addverb?retryWrites=true&w=majority'
-);
+mongoose.connect('mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app');
 
 module.exports = { mongoose };
